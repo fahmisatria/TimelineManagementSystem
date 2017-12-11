df33653b58f32a9e4dafc36dfcd53a9d828b2057
fahmisatria <fahmiaji@ymail.com>
2017-12-12 01:01:55 +0700

    update source code main.go

--- source code/main.go
+++ source code/main.go
@@ -1,197 +1,75 @@
 // Nama : Fahmi Satria Aji 
 // NIM  : 18215021
 // Tugas : II3160 Pemrograman Integratif: Web Service 
-// Deskripsi : File "tmshandler.go adalah file handler yang berisi fungsi 
-//dibutuhkan untuk menjalankan fungsi web service "timelinemanagementsystem" 
+// Deskripsi : File "main.go adalah file utama yang dibutuhkan
+// untuk menjalankan fungsi web service "timelinemanagementsystem" 
 
 package main 
 
 import (
-	"net/http"
-	"encoding/json"
-	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 	_ "github.com/go-sql-driver/mysql"
 )
-//Fungsi Untuk Mendapatkan Seluruh Kejaran Divisi
-func GetAllKejaran(rps http.ResponseWriter, rqs *http.Request) {
-	//Membuka koneksi ke database "timeline_management_system"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dbms.Close()
-	//Inisialisasi Data Kejaran 
-	div := DataKejaran{}
-	//Query untuk GetAllKejaranDivisi
-	baris, err := dbms.Query("select divisi.Nama_Divisi, pekerjaan.Nama_Pekerjaan, memiliki.Tanggal_Tenggat, memiliki.Penanggung_Jawab, memiliki.Deskripsi_Pekerjaan, memiliki.Kontak from memiliki inner join divisi, pekerjaan where memiliki.ID_Divisi = divisi.ID_Divisi and memiliki.ID_Pekerjaan = pekerjaan.ID_Pekerjaan")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer baris.Close()
-	//Scanning Rows
-	for baris.Next(){
-		err:= baris.Scan(&div.Nama_Divisi, &div.Nama_Pekerjaan, &div.Tanggal_Tenggat, &div.Penanggung_Jawab, &div.Deskripsi_Pekerjaan,&div.Kontak)
-		//Error Handling
-		if err != nil {
-			log.Fatal(err)
-		}
-		json.NewEncoder(rps).Encode(&div)
-	}
-	err=baris.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-//Fungsi Untuk Mendapatkan Kejaran Divisi Berdasarkan Nama Divisi
-func GetKejaranByNamaDivisi(rps http.ResponseWriter, rqs *http.Request, Nama_Divisi string) {
-	//Membuka koneksi ke database "timeline_management_system"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dbms.Close()
-	//Inisialisasi Data Kejaran Divisi
-	divisi := DataKejaranByDivisi{}
-	//Query untuk GetKejaranByNamaDivisi
-	baris, err := dbms.Query("select divisi.Nama_Divisi, pekerjaan.Nama_Pekerjaan, memiliki.Penanggung_Jawab, memiliki.Tanggal_Tenggat, memiliki.Kontak from memiliki inner join divisi, pekerjaan where memiliki.ID_Divisi = divisi.ID_Divisi and memiliki.ID_Pekerjaan = pekerjaan.ID_Pekerjaan and divisi.Nama_Divisi like?", "%"+Nama_Divisi+"%")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer baris.Close()
-	//Scanning Rows
-	for baris.Next(){
-		err:= baris.Scan(&divisi.Nama_Divisi, &divisi.Nama_Pekerjaan, &divisi.Penanggung_Jawab, &divisi.Tanggal_Tenggat, &divisi.Kontak)
-		//Error Handling
-		if err != nil {
-			log.Fatal(err)
-		}
-		json.NewEncoder(rps).Encode(&divisi)
-	}
-	err=baris.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-//Fungsi Untuk Mendapatkan Kejaran Divisi Berdasarkan Penanggung Jawab
-func GetKejaranByPenanggungJawab(rps http.ResponseWriter, rqs *http.Request, Penanggung_Jawab string) {
-	//Membuka koneksi ke database "timeline_management_system"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dbms.Close()
-	//Inisialisasi Data Kejaran By PenanggungJawab
-	pjb := DataKejaranByPenanggungJawab{}
-	//Query untuk GetKejaranByNamaDivisi
-	baris, err := dbms.Query("select divisi.Nama_Divisi, pekerjaan.Nama_Pekerjaan, divisi.Ketua_Divisi, divisi.Bidang, divisi.Ketua_Bidang, memiliki.Tanggal_Tenggat, memiliki.Kontak from memiliki inner join divisi, pekerjaan where memiliki.ID_Divisi = divisi.ID_Divisi and memiliki.ID_Pekerjaan = pekerjaan.ID_Pekerjaan and memiliki.Penanggung_Jawab like?", "%"+Penanggung_Jawab+"%")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer baris.Close()
-	//Scanning Rows
-	for baris.Next(){
-		err:= baris.Scan(&pjb.Nama_Divisi, &pjb.Nama_Pekerjaan, &pjb.Ketua_Divisi, &pjb.Bidang, &pjb.Ketua_Bidang, &pjb.Tanggal_Tenggat, &pjb.Kontak)
-		//Error Handling
-		if err != nil {
-			log.Fatal(err)
-		}
-		json.NewEncoder(rps).Encode(&pjb)
-	}
-	err=baris.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
-//Fungsi Untuk Mendapatkan Kejaran Divisi Berdasarkan Tanggal Tenggat
-func GetKejaranByTanggalTenggat(rps http.ResponseWriter, rqs *http.Request, Tanggal_Tenggat string) {
-	//Membuka koneksi ke database "timeline_management_system"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dbms.Close()
-	//Inisialisasi Data Kejaran By Tanggal Tenggat
-	tggltgt := DataKejaranByTanggalTenggat{}
-	//Query untuk GetKejaranByNamaDivisi
-	baris, err := dbms.Query("select divisi.Nama_Divisi, pekerjaan.Nama_Pekerjaan, divisi.Ketua_Divisi, divisi.Bidang, divisi.Ketua_Bidang, pekerjaan.Tanggal_Mulai, memiliki.Kontak from memiliki inner join divisi, pekerjaan where memiliki.ID_Divisi = divisi.ID_Divisi and memiliki.ID_Pekerjaan = pekerjaan.ID_Pekerjaan and memiliki.Tanggal_Tenggat like?", "%"+Tanggal_Tenggat+"%")
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer baris.Close()
-	//Scanning Rows
-	for baris.Next(){
-		err:= baris.Scan(&tggltgt.Nama_Divisi, &tggltgt.Nama_Pekerjaan, &tggltgt.Ketua_Divisi, &tggltgt.Bidang, &tggltgt.Ketua_Bidang, &tggltgt.Tanggal_Mulai, &tggltgt.Kontak)
-		//Error Handling
-		if err != nil {
-			log.Fatal(err)
+func main() {
+	port := 8181 //port yang digunakan untuk menjalankan web service
+	// Handle Function untuk menjalankan fungsionalitas GETKejaranDivisi By Nama Divisi, Penanggung Jawab, dan Tanggal_Tenggat
+	http.HandleFunc("/timelinemanagementsystem/", func (rps http.ResponseWriter, rqs *http.Request) {
+		switch rqs.Method {
+			//Method GET
+			case "GET":
+				//Mendapatkan Kejaran Divisi berdasarkan nama divisi
+				if rqs.URL.Query().Get("Nama_Divisi") != ""{
+					Nama_Divisi := rqs.URL.Query().Get("Nama_Divisi")
+					GetKejaranByNamaDivisi(rps,rqs,Nama_Divisi)
+					//Cek di bash
+					log.Printf("Masukan anda telah diterima, masukan:%s",Nama_Divisi)
+				//Mendapatkan Kejaran Divisi berdasarkan penanggung jawab 
+				}else if rqs.URL.Query().Get("Penanggung_Jawab") != ""{
+					Penanggung_Jawab := rqs.URL.Query().Get("Penanggung_Jawab")
+					GetKejaranByPenanggungJawab(rps,rqs,Penanggung_Jawab)
+					//Cek di bash
+					log.Printf("Masukan anda telah diterima, masukan:%s",Penanggung_Jawab)
+				//Mendapatkan Kejaran Divisi berdasarkan Tanggal Tenggat suatu pekerjaan
+				}else if rqs.URL.Query().Get("Tanggal_Tenggat") != ""{
+					Tanggal_Tenggat := rqs.URL.Query().Get("Tanggal_Tenggat")
+					GetKejaranByTanggalTenggat(rps,rqs,Tanggal_Tenggat)
+					//Cek di bash
+					log.Printf("Masukan anda telah diterima, masukan:%s", Tanggal_Tenggat)
+				//Mendapatkan Seluruh Kejaran Divisi
+				}else {
+					GetAllKejaran(rps,rqs)
+					log.Printf("Berhasil")
+				}
+				//Membuat data kejaran baru pada tabel memiliki 
+			default:
+				http.Error(rps, "invalid", 405) //mengirimkan respons error
+
 		}
-		json.NewEncoder(rps).Encode(&tggltgt)
-	}
-	err=baris.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	})
+	//Handle Function untuk melakukan POSTPekerjaan
+	http.HandleFunc("/postpekerjaan/", func (rps http.ResponseWriter, rqs *http.Request) {
+		switch rqs.Method {
+			case "POST":
+				//POST Pekerjaan To Tabel Pekerjaan
+				PostPekerjaan(rps,rqs)
+		default:
+			http.Error(rps, "invalid", 405) //mengirimkan respons error
+		}
+	})
+	//Handle Function untuk melakukan POSTDivisiMemilikiPekerjaan
+	http.HandleFunc("/postmemilikipekerjaan/", func (rps http.ResponseWriter, rqs *http.Request) {
+		switch rqs.Method {
+			case "POST":
+				//POST Pekerjaan To Tabel Pekerjaan
+				PostMemilikiPekerjaan(rps,rqs)
+		default:
+			http.Error(rps, "invalid", 405) //mengirimkan respons error
+		}
+	})
+	//Menampilkan pesan pada client bahwa web service dapat berjalan
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v",port),nil))
 }
-
-//Fungsi Untuk melakukan POST Data Kejaran Divisi ke tabel memiliki
-func PostPekerjaan (rps http.ResponseWriter, rqs *http.Request) {
-	var pekerjaanbaru Pekerjaan
-	x := json.NewDecoder(rqs.Body)
-	err:= x.Decode(&pekerjaanbaru)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rqs.Body.Close()
-	//Membuka koneksi ke database "timelinemanagementsystem"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil{
-		log.Fatal(err)
-	}
-	//Query untuk melakukan insert into tabel pekerjaan 
-	baris,err := dbms.Prepare("insert into pekerjaan (ID_Pekerjaan,Nama_Pekerjaan,Tanggal_Mulai,Tanggal_Selesai,Penanggung_Jawab) VALUES (?,?,?,?,?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_,err = baris.Exec(pekerjaanbaru.ID_Pekerjaan, pekerjaanbaru.Nama_Pekerjaan, pekerjaanbaru.Tanggal_Mulai, pekerjaanbaru.Tanggal_Selesai, pekerjaanbaru.Penanggung_Jawab)
-}
-
-//Fungsi Untuk melakukan POST Data Memiliki Kejaran ke tabel memiliki
-func PostMemilikiPekerjaan (rps http.ResponseWriter, rqs *http.Request) {
-	var memilikipekerjaan MemilikiPekerjaan 
-	x := json.NewDecoder(rqs.Body)
-	err:= x.Decode(&memilikipekerjaan)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rqs.Body.Close()
-	//Membuka koneksi ke database "timelinemanagementsystem"
-	dbms, err := sql.Open("mysql",
-			  "root:@tcp(127.0.0.1:3306)/timeline_management_system")
-	//Error Handling
-	if err != nil{
-		log.Fatal(err)
-	}
-	//Query untuk melakukan insert to tabel memiliki
-	baris,err := dbms.Prepare("insert into memiliki (ID_Divisi,ID_Pekerjaan,Nama_Divisi,Tanggal_Tenggat,Penanggung_Jawab,Deskripsi_Pekerjaan,Kontak) VALUES (?,?,?,?,?,?,?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_,err = baris.Exec(memilikipekerjaan.ID_Divisi, memilikipekerjaan.ID_Pekerjaan, memilikipekerjaan.Nama_Divisi, memilikipekerjaan.Tanggal_Tenggat, memilikipekerjaan.Penanggung_Jawab,memilikipekerjaan.Deskripsi_Pekerjaan, memilikipekerjaan.Kontak)
-}
-
